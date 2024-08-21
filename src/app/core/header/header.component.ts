a911e86674982340895bc01b6fe0da8d73ba5352
Gabriel <gabrielleong917@gmail.com>
2024-08-21 12:01:01 -0700

    Added buger menu for mobile nav

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,4 +8,33 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  screenWidth = window.innerWidth;
+  isMenuOpen = false;
+  isMobileView = false;
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+
+    if (window.innerWidth < 640) {
+      this.isMobileView = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.screenWidth = window.innerWidth;
+
+    if (window.innerWidth < 640) {
+      this.isMobileView = true;
+    }
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
